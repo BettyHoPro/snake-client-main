1d77477691b98bc89c6c1511ccfaf371998fae3e
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-10 14:38:48 -0800

    add the wasd keys to print out messages in terminal

--- input.js
+++ input.js
@@ -6,6 +6,14 @@ const setupInput = () => {
     stdin.on('data', (key) => {
       if (key === '\u0003') {
         process.exit();
+      } else if (key === "w") {
+        console.log("up");
+      } else if (key === "a") {
+        console.log("left");
+      } else if (key === "s") {
+        console.log("down");
+      } else if (key === "d") {
+        console.log("right");
       }
     });
   };
